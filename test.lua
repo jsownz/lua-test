04619d37c2ccf007e2344f394079c18ed196cdd5
Jsownz <jason@frozensoliddesigns.com>
2018-07-23 14:47:23 -0700

    script detects aggro and losing aggro

--- test.lua
+++ test.lua
@@ -1,1 +1,12 @@
-message("test")
+local Aggro_EventFrame = CreateFrame("Frame")
+Aggro_EventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+local unAggro_EventFrame = CreateFrame("Frame")
+unAggro_EventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
+Aggro_EventFrame:SetScript("OnEvent",
+    function(self, event)
+	    print('Got aggro...')
+    end)
+unAggro_EventFrame:SetScript("OnEvent",
+    function(self, event)
+	    print('Lost aggro...')
+	end)
